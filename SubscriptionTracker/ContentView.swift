7284f4767aaa3b089c214183b81c32f2cb8c6dfe
Hassan Haider <zyrox762@gmail.com>
2026-09-25 02:09:39 -0700

    Initial Commit

---
+++ SubscriptionTracker/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  SubscriptionTracker
+//
+//  Created by !---------? on 25/09/2026.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ SubscriptionTracker/SubscriptionTrackerApp.swift
@@ -0,0 +1,17 @@
+//
+//  SubscriptionTrackerApp.swift
+//  SubscriptionTracker
+//
+//  Created by !---------? on 25/09/2026.
+//
+
+import SwiftUI
+
+@main
+struct SubscriptionTrackerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
